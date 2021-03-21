142326dceb53d8a2e939f4492c495c349e6f1acb
p-vaibhav <80158688+p-vaibhav@users.noreply.github.com>
2021-03-21 17:00:03 +0530

    Add files via upload

---
+++ calc.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+int option;
+double num1, num2;
+double result;
+
+cout << "Calculator" << endl <<endl << endl;
+cout << "1. Addition" << endl;
+cout << "2. Subtraction" << endl;
+cout << "3. Multiplication" << endl;
+cout << "4. Division" << endl;
+cout << "5. Square" << endl;
+cout << endl << "Enter an option" << endl;
+cin >> option;
+
+switch (option)
+{
+case 1:
+    {
+        cout << "Enter 1st number" << endl;
+        cin >> num1;
+        cout << "Enter 2nd number" << endl;
+        cin>> num2;
+        result = num1 + num2;
+        cout << "Result = " << result;
+    break;
+    }
+case 2:
+    {
+        cout << "Enter 1st number" << endl;
+        cin >> num1;
+        cout << "Enter 2nd number" << endl;
+        cin>> num2;
+        result = num1 - num2;
+        cout << "Result = " << result;
+    break;
+    }
+case 3:
+    {
+        cout << "Enter 1st number" << endl;
+        cin >> num1;
+        cout << "Enter 2nd number" << endl;
+        cin>> num2;
+        result = num1 * num2;
+        cout << "Result = " << result;
+    break;
+    }
+case 4:
+    {cout << "Enter 1st number" << endl;
+        cin >> num1;
+        cout << "Enter 2nd number" << endl;
+        cin>> num2;
+        result = num1 / num2;
+        cout << "Result = " << result;
+    break;
+    }
+case 5:
+    {cout << "Enter a number" << endl;
+        cin >> num1;
+        result = num1^2;
+        cout << "Result = " << result;
+    break;
+    }
+
+default:
+    {
+        cout << "Error!!! You entered a wrong option." <<endl;
+        cout << "Please retry." << endl;
+    }
+    break;
+}
+}
+
